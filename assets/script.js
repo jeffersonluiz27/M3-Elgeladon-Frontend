7e01a5010eb3851972c816a8a0b954c9d2a9a5ba
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-04-22 20:29:12 -0300

    reformulação titulo menu sacola

--- assets/script.js
+++ assets/script.js
@@ -1,64 +1,69 @@
 const baseUrl = "http://localhost:3000/paletas";
 
 async function findAllPaletas() {
-  const response = await fetch(`${baseUrl}/find-paletas`);
+	const response = await fetch(`${baseUrl}/find-paletas`);
 
-  const paletas = await response.json();
+	const paletas = await response.json();
 
-  if(paletas.types == 'erro'){
-    mensagem(paletas.message,'erro')
-  }
+	if (paletas.types == 'erro') {
+		mensagem(paletas.message, 'erro');
+	}
 
-  paletas.forEach((paleta) => {
-    document.getElementById("contentList").insertAdjacentHTML(
-      "beforeend",
-      `<div class="paletaItem" id="paletaItem_${paleta.id}">
+	paletas.forEach((paleta) => {
+		document.getElementById('contentList').insertAdjacentHTML(
+			'beforeend',
+			`<div class="paletaItem" id="paletaItem_${paleta.id}">
         <div>
             <div class="paletaItem__sabor">${paleta.sabor}</div>
             <div class="paletaItem__preco">R$ ${paleta.preco}</div>
             <div class="paletaItem__descricao">${paleta.descricao}</div>
             <div class="paletaItem__acoes">
               <div class="acoes">
-                <button class="acoes__editar btn" onclick="abrirModal(${paleta.id})">Editar</button> 
-                <button class="acoes__apagar btn" onclick="abrirModalDelete(${paleta.id})">Apagar</button> 
+                <button class="acoes__editar btn" onclick="abrirModal(${
+									paleta.id
+								})">Editar</button> 
+                <button class="acoes__apagar btn" onclick="abrirModalDelete(${
+									paleta.id
+								})">Apagar</button> 
               </div>
             </div>
         </div>
             <img class="paletaItem__img" src=${
-              paleta.foto
-            } alt=${`Paleta de ${paleta.sabor}`} />
+							paleta.foto
+						} alt=${`Paleta de ${paleta.sabor}`} />
             
       </div>`
-    );
-  });
-};
+		);
+	});
+}
 
 findAllPaletas();
 
 const findPaletaById = async () => {
+	const id = document.getElementById('idPaleta').value;
 
-  const id = document.getElementById("idPaleta").value;
+	console.log(id);
 
-  if(id <= 0){
-    mensagem('Digite um numero válido!','erro');
-  }
-  document.querySelector(".modal-overlay1").style.display = "flex";
+	if (id <= 0) {
+		mensagem('Digite um numero válido!', 'erro');
+	}
+	document.querySelector('.modal-overlay1').style.display = 'flex';
 
-  const response = await fetch(`${baseUrl}/find-paleta/${id}`);
+	const response = await fetch(`${baseUrl}/find-paleta/${id}`);
 
-  const paleta = await response.json();
+	const paleta = await response.json();
 
-  if(paleta.id == undefined){
-    const paletaEscolhidaDiv = document.getElementById("paletaEscolhida");
-    paletaEscolhidaDiv.innerHTML = `<div class="paletaItem">
+	if (paleta.id == undefined) {
+		const paletaEscolhidaDiv = document.getElementById('paletaEscolhida');
+		paletaEscolhidaDiv.innerHTML = `<div class="paletaItem">
     <div>
         <div class="paletaItem__sabor">Paleta não encontrada</div>
-    </div>`
-    mensagem('Digite um numero válido!','erro');
-  }else{
-    const paletaEscolhidaDiv = document.getElementById("paletaEscolhida");
+    </div>`;
+		mensagem('Digite um numero válido!', 'erro');
+	} else {
+		const paletaEscolhidaDiv = document.getElementById('paletaEscolhida');
 
-    paletaEscolhidaDiv.innerHTML = `
+		paletaEscolhidaDiv.innerHTML = `
     <div class="paletaItem" id="paletaItem_${paleta.id}">
       <div>
           <div class="paletaItem__sabor">${paleta.sabor}</div>
@@ -66,10 +71,10 @@ const findPaletaById = async () => {
           <div class="paletaItem__descricao">${paleta.descricao}</div>
         </div>
           <img class="paletaItem__img" src=${
-            paleta.foto
-          } alt=${`Paleta de ${paleta.sabor}`} />
+						paleta.foto
+					} alt=${`Paleta de ${paleta.sabor}`} />
     </div>`;
-  }
+	}
 };
 
 function fecharModal() {
